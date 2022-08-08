4adf407a8d53b7584ef0f3228ec673fa39a0eb20
ManoharNettem <manohar@elpaviation.com>
2022-08-08 18:15:24 +0530

    removing app center

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.flutter_app;
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-        AppCenter.start(getApplication(), "628b8f76-091f-434a-bb6a-1a9e4b9184b9",
-                Analytics.class, Crashes.class);
+
 }
