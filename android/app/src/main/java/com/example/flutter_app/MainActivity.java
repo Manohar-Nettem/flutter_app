e2cb82496bb56e3a14bfb0f97867db53ad0675aa
ManoharNettem <manohar@elpaviation.com>
2022-08-09 18:26:32 +0530

    super

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends FlutterActivity {
         
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+        super.onCreate(savedInstanceState);
         AppCenter.start(getApplication(), "e8aa9dbc-dea5-4e47-b543-f3c9634c3da5",
                     Analytics.class, Crashes.class);
     }
