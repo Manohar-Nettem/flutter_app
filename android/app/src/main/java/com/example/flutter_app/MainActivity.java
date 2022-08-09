d1c4d2dfed958f87eb8952e8981845f7abfa16e3
ManoharNettem <manohar@elpaviation.com>
2022-08-09 15:09:30 +0530

    changes

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.flutter_app;
-
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+        
+    AppCenter.start(getApplication(), "e8aa9dbc-dea5-4e47-b543-f3c9634c3da5",
+                  Analytics.class, Crashes.class);
 
+        
 }
